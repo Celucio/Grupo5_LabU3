54433b21c575f531255079d2b102e48466e59ac5
LuisOlalla <75482079+LuisOlalla@users.noreply.github.com>
2022-02-14 21:16:30 -0500

    eskeler

--- Ejercicio1/app.js
+++ Ejercicio1/app.js
@@ -1,12 +1,8 @@
-// console.log('TODO FUNCIONA BIEN');
+
 const sectForm = document.querySelector('#form');
 const cambio = document.querySelector('#cambio');
 const hasColors = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-/*for ($i = 0; $i < 6; $i++) {
-    $dato = $_POST["element-color$i"];
-    $hexColor = $hexColor . $dato;
-    // echo $hexColor;
-};*/
+
 // CARGAR LOS SELECT Y OPTIONS EN LA PAGINA
 for (let i = 0; i < 6; i++) {
     let newDiv = document.createElement('div');
@@ -18,7 +14,6 @@ for (let i = 0; i < 6; i++) {
     newSelect.id = `element-color${i}`;
     for (item of hasColors) {
         let newOption = document.createElement('option');
-        // console.log(item)
         newOption.value = item;
         newOption.text = item;
         newSelect.appendChild(newOption);
@@ -30,7 +25,6 @@ for (let i = 0; i < 6; i++) {
 }
 
 let newInput = document.createElement('INPUT');
-// newInput.type = "submit";
 newInput.setAttribute("type", "submit");
 newInput.className = "btn1"
 newInput.id = "btn"
@@ -38,19 +32,7 @@ newInput.name = "btnFinal"
 newInput.value = 'GENERAR COLOR'
 sectForm.appendChild(newInput);
 
-{/* <input type="submit" class="btn1" id="btn" name="btnFinal" value="GENERAR COLOR" ></input> */ }
-// const btn = document.getElementById("btn");
-// console.log(btn);
 
-// btn.addEventListener("click", function () {
-//     let hexColor = '#';
-//     for (let i = 0; i < 6; i++) {
-//         hexColor += document.getElementById(`element-color${i}`).value;;
-//     }
 
-//     color.textContent = hexColor;
-//     document.body.style.backgroundColor = hexColor;
-//     console.log(hexColor)
-// });
 
 
